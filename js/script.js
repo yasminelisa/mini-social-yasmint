55089023781476ba9654a90fff58e98addff4be4
yasminelisa <yasmin.7555966@aluno.mg.gov.br>
2026-04-01 13:55:35 +0000

    update js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 let likeCount = 0;
-let curtido = false; // flag booleana
+let curtido = false; //flag booleana
 
 function curtir() {
-  if(curtido == false) {
+
+  if(curtido == false){
     likeCount++;
     curtido = true;
-    document.ElementById("likeCount").innerText = likeCount;
+    document.getElementById("likeCount").innerText = likeCount;
   }else{
     likeCount--;
     curtido = false;
-    document.ElementById("likeCount").innerText = likeCount;
+    document.getElementById("likeCount").innerText = likeCount;
   }
-} 
+
+
+}
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
